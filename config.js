dc4bd637c722c93d30b30213982f2e77d5d507c9
jfmartinez4 <jmartine@ciesin.columbia.edu>
2026-02-12 13:49:28 -0500

    removed path prefix

--- config.js
+++ config.js
@@ -35,7 +35,7 @@ export default {
     displayOverview: true,
     buildTileUrlTemplate: null,
     getMapSourceOptions: null,
-    pathPrefix: "/stac-browser/",
+    pathPrefix: "",
     historyMode: "history",
     cardViewMode: "cards",
     cardViewSort: "asc",
